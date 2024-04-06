cd4bc075938ab9814c7c2d802aa9338afbdccb03
zinmyoswe <zinmyoswe@github.com>
2024-04-06 16:47:45 +0630

    NodeAPI finish

--- node.sh
+++ node.sh
@@ -8,3 +8,25 @@ expressjs is nodejs framework, it can run and code node application easily
 
 #installation expressjs
 npm i express
+
+#install nodemon beacuse not to run server again when we change something
+npm i nodemon -D
+
+#connect to database MongoDB
+npm i mongoose
+
+
+
+-------------------
+echo "# Nodejs-API-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/zinmyoswe/Nodejs-API-.git
+git push -u origin main
+
+
+git remote add origin https://github.com/zinmyoswe/Nodejs-API-.git
+git branch -M main
+git push -u origin main
--- server.js
+++ server.js
@@ -1,12 +1,96 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 //routes
 
 app.get('/', (req, res) =>{
     res.send('Hello')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API');
+app.get('/blog', (req, res) =>{
+    res.send('Blog')
+})
+
+//Show all Product
+app.get('/products', async(req, res) =>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Get Product By ID
+app.get('/products/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const products = await Product.findById(id);
+        res.status(200).json(products);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//update a product
+app.put('/products/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        //cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: 'cannnot find any product with ID ${id}'})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//delete a product
+app.delete('/products/:id', async(req, res) =>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        //cannot find any product in database
+        if(!product){
+            return res.status(404).json({message: 'cannnot find any product with ID ${id}'})
+        }
+        res.status(200).json(product);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+//Create Product
+app.post('/products', async(req, res) =>{
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+mongoose.set("strictQuery",false)
+mongoose.connect('mongodb+srv://zinmyoswe:12345Zinmyoswe@zinmyosweapi.s8bbjws.mongodb.net/?retryWrites=true&w=majority&appName=zinmyosweAPI')
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(3000, ()=> {
+        console.log('Node API is running on port 3000');
+    })
+    
+}).catch((error) => {
+    console.log(error)
 })
